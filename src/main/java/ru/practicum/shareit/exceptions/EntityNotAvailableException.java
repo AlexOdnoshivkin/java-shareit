215865c207aea1a81aa480b030f4cc0bb796ea10
Alex Odnoshivkin <alexander.odnoshivkin@yandex.ru>
2022-10-28 10:40:07 +0300

    feat: add-docker

--- src/main/java/ru/practicum/shareit/exceptions/EntityNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class EntityNotAvailableException extends RuntimeException {
-    public EntityNotAvailableException(String message) {
-        super(message);
-    }
-}
